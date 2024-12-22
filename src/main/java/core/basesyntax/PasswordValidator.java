aa03d7880dc161e67ffa168a42f3653be4d39d9a
Oleksii Vitiuk <vitiuk.oleksii@gmail.com>
2024-12-22 17:35:59 +0200

    done 1.1

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class PasswordValidator {
     public void validate(String password, String repeatPassword)
             throws PasswordValidationException {
-        //write your code here
+        //write your code here///
         if (password == null || repeatPassword == null) {
             throw new PasswordValidationException("Passwords cannot be null.");
         }
